917289a8ac0178d0ee87056e2b166daa9c32b24c
cdog <cdog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-09 21:11:04 +0000

    Tagged version 1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/avatar-manager/tags/1.5.0@738466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -6,24 +6,24 @@
 
 // Exits if uninstall is not called from WordPress.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit;
+    exit;
 
 if ( ! function_exists( 'avatar_manager_delete_avatar' ) )
-	include_once( 'avatar-manager.php' );
+    include_once( 'avatar-manager.php' );
 
 // Deletes plugin options.
 delete_option( 'avatar_manager' );
 
 // An associative array with criteria to match.
 $args = array(
-	'meta_key' => 'avatar_manager_custom_avatar'
+    'meta_key' => 'avatar_manager_custom_avatar'
 );
 
 // Retrieves an array of users matching the criteria given in $args.
 $users = get_users( $args );
 
 foreach ( $users as $user ) {
-	// Deletes avatar image based on attachment ID.
-	avatar_manager_delete_avatar( $user->avatar_manager_custom_avatar );
+    // Deletes avatar image based on user ID.
+    avatar_manager_delete_avatar( $user->ID );
 }
 ?>
